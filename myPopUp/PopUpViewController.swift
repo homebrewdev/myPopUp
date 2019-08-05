0e2dbafd2f2dcd9c0074f043185b375dba3f0df8
Edeviatov <edeviatov@htc-cs.ru>
2019-08-05 17:35:36 +0400

    - сделал кастомные alertController с анимацией

---
+++ myPopUp/PopUpViewController.swift
@@ -0,0 +1,64 @@
+//
+//  PopUpViewController.swift
+//  myPopUp
+//
+//  Created by Egor Devyatov on 05.08.2019.
+//  Copyright © 2019 Egor Devyatov. All rights reserved.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    @IBOutlet weak var messageView: UIView!
+    
+    @IBOutlet weak var alertTextView: UITextView!
+    
+    let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        messageView.layer.cornerRadius = 24 // зададим радиус скругления
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.75) // затеняем главый вью контроллера
+        // анимашка появления/открытия алерта
+        moveIn()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // по нажатию на кнопку закрыть
+    @IBAction func closePopUPButtonTap(_ sender: UIButton) {
+        //self.view.removeFromSuperview()
+        // выкидываем наш попап контрол с контроллера с анимацией
+        moveOut()
+    }
+    
+    // функция передачи тела сообщения в наш алерт
+    func alertActivate(_ message: String, _ color: UIColor) {
+        alertTextView.text = message
+        alertTextView.textColor = color
+    }
+    
+    // анимашка для открытия алерта
+    func moveIn() {
+        self.view.transform = CGAffineTransform(scaleX: 1.35, y: 1.35)
+        self.view.alpha = 0.0
+        
+        UIView.animate(withDuration: 0.24) {
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            self.view.alpha = 1.0
+        }
+    }
+    // анимашка для закрытия алерта
+    func moveOut() {
+        UIView.animate(withDuration: 0.24, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.35, y: 1.35)
+            self.view.alpha = 0.0
+        }) { _ in
+            // выкидываем наш попап контрол с контроллера
+            self.view.removeFromSuperview()
+        }
+    }
+}
--- myPopUp/ViewController.swift
+++ myPopUp/ViewController.swift
@@ -12,9 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func showPopUPButtonTap(_ sender: UIButton) {
+//       1.  Мы получаем ссылку на PopUpViewController, причем получаем ссылку мы при помощи метода, который обращается к StoryBoard и находит контроллер, у которого идентификатор popUpVCid.
+//       2. Добавляем полученный контроллер PopUpViewController в качестве дочернего к ViewController.
+//       3. Размеры View нового контроллера мы устанавливаем равными размера View текущего контроллера.
+//       4. Непосредственно выводим на экран то,  как отображается PopUpViewController. Т.е. добавляем View PopUpViewController в стек той кучи View, которые уже есть.
+//       5. Вызываем метод делегата, чтобы сообщить всем делегатам, о том, что контроллер мы отобразили.
+        let popUpVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "popUpVCid") as! PopUpViewController // 1
+        
+        self.addChild(popUpVC) // 2
+        popUpVC.view.frame = self.view.frame  // 3
+        self.view.addSubview(popUpVC.view) // 4
+        
+        popUpVC.didMove(toParent: self) // 5
+        popUpVC.alertActivate(popUpVC.loremIpsum, UIColor.black)
+    }
+    
 }
 
